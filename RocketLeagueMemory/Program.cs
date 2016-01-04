0bc408f46df1c7e66d5211619645dc03b9590b21
Logan Saso <jewsofhazard@gmail.com>
2016-01-03 22:28:59 -0800

    Redid CololKeyboard.cs to be not as shitty. Updated for RL patch 1.10.

--- RocketLeagueMemory/Program.cs
+++ RocketLeagueMemory/Program.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using System.Drawing;
 using CUE.NET.Devices.Keyboard;
+using CUE.NET.Brushes;
 using CUE.NET;
 using CUE.NET.Exceptions;
 
@@ -12,27 +13,29 @@ namespace RocketLeagueMemory
        
         static void Main(string[] args)
         {
+
+            string memAddress = "\"RocketLeague.exe\"+014FA9C4+58+5AC+6F4+21C";
             var rlProcess = Process.GetProcessesByName("RocketLeague");
             Memory.Memory mem = new Memory.Memory(rlProcess[0]);
-            IntPtr boostAddress = mem.GetAddress("\"RocketLeague.exe\"+01570020+94+204+6F4+21C");
+            IntPtr boostAddress = mem.GetAddress(memAddress);
             var boostFloat = mem.ReadFloat(boostAddress) / 3 * 100;
 
             CueSDK.Initialize();
             CorsairKeyboard keyboard = CueSDK.KeyboardSDK;
             if (keyboard == null)
                 throw new WrapperException("No keyboard found.");
-            keyboard.Color = Color.Black;
-            keyboard.UpdateLeds();
+            keyboard.Brush = new SolidColorBrush(Color.Black);
+            keyboard.Update();
 
-            Corsair.ColorKeyboard.colorBoard(100, keyboard,  0);
+            Corsair.ColorKeyboard.ColorTheBoard(0, keyboard);
 
 
             while (true)
             {
-                boostAddress = mem.GetAddress("\"RocketLeague.exe\"+01570020+94+204+6F4+21C");
+                boostAddress = mem.GetAddress(memAddress);    
                 boostFloat = mem.ReadFloat(boostAddress) / 3 * 100;
-                Console.WriteLine((int)boostFloat);
-                Corsair.ColorKeyboard.colorBoard((int)boostFloat, keyboard,  1);
+                Console.WriteLine(boostFloat);
+                Corsair.ColorKeyboard.ColorTheBoard(boostFloat, keyboard);
             }
         }
     }
